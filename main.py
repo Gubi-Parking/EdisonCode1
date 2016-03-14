c12663078a21aba9bb23f7cbd021a7f4a699def3
Fernando Chavez <fertux200795@hotmail.com>
2016-03-14 16:36:09 -0600

    Hola

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ button2 = grove.GroveButton(3)
 count = 0;
 
 check = True
+pressed=False
 mylcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 mylcd.setColor(0,0,255)
 
@@ -17,19 +18,26 @@ mylcd.setColor(0,0,255)
 while 1:
 
   if button1.value() and check:
-    mylcd.clear()
-    mylcd.setCursor(0,0)
-    count +=1
-    print(count)
-    mylcd.write(str(count))
     check = False
+    if pressed:
+      count+=1
+      pressed= False
+      mylcd.clear()
+      mylcd.setCursor(0,0)
+      mylcd.write(str(count))
+    else:
+      pressed=True
   if button2.value() and check:
-    mylcd.clear()
-    mylcd.setCursor(0,0)
-    count -=1
-    print(count)
-    mylcd.write(str(count))
+    
     check = False
+    if pressed:
+      count-=1
+      pressed= False
+      mylcd.clear()
+      mylcd.setCursor(0,0)
+      mylcd.write(str(count))
+    else:
+      pressed=True
   elif not(button1.value()) and not(check) and not (button2.value()):
     check = True
 
