61ca5fc4fc5c0f1703f7cbce2d01192596a984ae
Fernando Chavez <fertux200795@hotmail.com>
2016-03-14 13:45:30 -0600

    Primer

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import pyupm_grove as grove
 import pyupm_buzzer as upmBuzzer
 import pyupm_i2clcd as lcd
+import time
 
 button = grove.GroveButton(4)
 
@@ -16,5 +17,6 @@ while 1:
       print("Prueba")
       count+=1
       print(count)
+      time.sleep(1)
 
 del button
