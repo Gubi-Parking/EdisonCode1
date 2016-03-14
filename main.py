e038d0e30d5437b505577c0623ecf6139b35393e
Fernando Chavez <fertux200795@hotmail.com>
2016-03-14 13:40:49 -0600

    Primer

--- main.py
+++ main.py
@@ -4,22 +4,15 @@ import pyupm_grove as grove
 import pyupm_buzzer as upmBuzzer
 import pyupm_i2clcd as lcd
 
-button = grove.GroveButton(3)
+button = grove.GroveButton(4)
+
+if button.
 
 
-myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
 while 1:
-    count = 0
-    myLcd.setCursor(0, 0)
-    myLcd.write(count);
     
     if button.value():
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        myLcd.setColor(r, g, b)
-        buzzer.playSound(chords[ran])
-        count = count +1
+      print("Prueba")
 
 del button
