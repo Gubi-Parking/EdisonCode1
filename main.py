f07612cb2397e97b9708738348c214d4760be947
Fernando Chavez <fertux200795@hotmail.com>
2016-03-14 15:48:58 -0600

    Hola

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ button = grove.GroveButton(4)
 count = 0;
 
 check = True
-
+mylcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+mylcd.setColor(0,0,255)
 
 
 while 1:
 
   if button.value() and check:
+    mylcd.setCursor(0,0)
     count +=1
     print(count)
+    mylcd.write(str(count))
     check = False
   elif not(button.value()) and not(check):
     check = True
